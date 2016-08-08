8de32a934404645be2d9b7c312f77012562de5b6
Stevie <steviepinero@gmail.com>
2016-08-08 11:57:10 -0400

    add card and upload form to pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -69,6 +69,6 @@ class PicturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def picture_params
-      params.fetch(:picture, {})
+      params.require(:picture)
     end
 end
